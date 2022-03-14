69b32877e9236e1d04155966ed1bce84804efeda
ThwetHumeNyein30 <thwethmuenyein.thn@gmail.com>
2022-03-14 16:06:21 +0630

    Commit for setup network module and di

---
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -0,0 +1,12 @@
+package com.example.foody.data.network
+
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+
+    @GET
+    suspend fun getRecipes(@QueryMap queries:Map<String,String>): Response<FoodRecipe>
+}
---
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foody.util
+
+class Constants {
+    companion object {
+        const val API_KEY= "8c338d3a9c474301a4bb515ed9840145"
+        const val BASE_URL="https://api.spoonacular.com"
+    }
+}
